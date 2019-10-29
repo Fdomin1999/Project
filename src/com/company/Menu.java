19aa29f0886d25f6f94d3d4cb3d7a60e4cdeb41c
dam2a <fdomin@elpuig.xeill.net>
2019-10-29 16:24:20 +0100

    iydfdf

---
+++ src/com/company/Menu.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    void lanzarMenu(){
+
+
+
+
+        System.out.println("CURSA: ");
+        System.out.println();
+        System.out.println();
+
+
+       Scanner sc = new Scanner;
+        int opcion sc.nextInt();
+        switch (opcion) {
+            case 1:
+
+
+                return;
+
+        }
+    }
+}
+}
